dbc333089a78c3f1eca6551d71b6e0875dcbbcb4
Roberto García Romero <92rober@gmail.com>
2024-01-10 23:18:41 +0100

    feat: onboarding screen almost implemented

--- androidApp/src/main/java/com/snap/fosdem/android/screens/OnBoardingRoute.kt
+++ androidApp/src/main/java/com/snap/fosdem/android/screens/OnBoardingRoute.kt
@@ -1,21 +1,89 @@
 package com.snap.fosdem.android.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.snap.fosdem.android.R
 import com.snap.fosdem.app.viewModel.OnBoardingViewModel
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun OnBoardingRoute(
     viewModel: OnBoardingViewModel = koinViewModel(),
     onNavigate: () -> Unit
 ) {
-    Column {
-        Text(text = "OnBoarding")
-        Button(onClick = { onNavigate() }) {
-            Text(text = "Next")
+    val pagerState = rememberPagerState(pageCount = {
+        3
+    })
+    Box(
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        HorizontalPager(
+            modifier = Modifier.fillMaxSize(),
+            state = pagerState
+        ) { page ->
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    modifier = Modifier.size(dimensionResource(id = R.dimen.splash_image_size)),
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = stringResource(R.string.fosdem_logo_description)
+                )
+                Text(
+                    text = "Page: $page",
+                )
+                if(page == pagerState.pageCount - 1) {
+                    Button(onClick = { onNavigate() }) {
+                        Text("Continuar")
+                    }
+                }
+            }
+        }
+        Row(
+            Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .padding(bottom = 32.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            repeat(pagerState.pageCount) { iteration ->
+                val color = if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                Box(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(8.dp)
+                )
+
+            }
         }
     }
 }
